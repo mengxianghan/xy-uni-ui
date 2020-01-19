af484979bc48741488b621bb751193962d8a454d
mengxianghan <1056811341@qq.com>
2020-01-19 13:55:49 +0800

    增加版本检查更新

--- App.vue
+++ App.vue
@@ -2,6 +2,33 @@
 export default {
 	onLaunch: function() {
 		// console.log('App Launch');
+		// #ifdef MP
+		if (uni.canIUse('getUpdateManager')) {
+			const updateManager = uni.getUpdateManager();
+
+			updateManager.onCheckForUpdate(function(res) {
+				// 请求完新版本信息的回调
+				console.log(res.hasUpdate);
+			});
+
+			updateManager.onUpdateReady(function() {
+				uni.showModal({
+					title: '更新提示',
+					content: '新版本已经准备好，是否重启应用？',
+					success: function(res) {
+						if (res.confirm) {
+							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+							updateManager.applyUpdate();
+						}
+					}
+				});
+			});
+
+			updateManager.onUpdateFailed(function() {
+				// 新版本下载失败
+			});
+		}
+		// #endif
 	},
 	onShow: function() {
 		// console.log('App Show');
@@ -18,14 +45,14 @@ export default {
 page {
 	background: #f8f8f8;
 }
-uni-page-head{
+uni-page-head {
 	font-family: 'PingFang SC';
 }
-.uni-page-head-btn{
+.uni-page-head-btn {
 	line-height: 1;
 }
 
 .demo-section {
-    padding-bottom: 20px;
+	padding-bottom: 20px;
 }
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -190,7 +190,8 @@ export default {
 				}
 			]
 		};
-	}
+	},
+	onShareAppMessage() {}
 };
 </script>
 
